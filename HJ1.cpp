771fb594d32575ed7b90ced36b4d161185d47fe3
yangc2023 <yangchuang_2016@163.com>
2023-07-01 22:46:01 +0800

    我的第一

---
+++ HJ1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    while (cin >> a >> b) { // 注意 while 处理多个 case
+        cout << a + b << endl;
+        // cout << "请继续输入：" <<endl;
+    }
+}
+// 64 位输出请用 printf("%lld")
